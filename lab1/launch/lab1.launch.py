37dc999076d9f870c937da38594354a11a1d2554
Jan Jeschke <jasiekjeschke@gmail.com>
2021-03-08 15:23:17 +0100

    added launch file

---
+++ lab1/launch/lab1.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='lab1',
+            executable='my_teleop',
+            name='my_teleop',
+            output='screen',
+        )
+    ])
