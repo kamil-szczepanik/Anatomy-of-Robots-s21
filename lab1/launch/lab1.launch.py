d0aa631410441d6b2c2ec1867fe67aab71285c4b
Jan Jeschke <jasiekjeschke@gmail.com>
2021-03-16 09:41:58 +0100

    launch works now

--- lab1/launch/lab1.launch.py
+++ lab1/launch/lab1.launch.py
@@ -2,17 +2,18 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='lab1',
-            executable='my_teleop',
-            name='my_teleop',
-            output='screen',
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package='turtlesim',
+                executable='turtlesim_node'
+            ),
+            Node(
+                package='lab1',
+                executable='my_teleop',
+                name='my_teleop',
+                prefix=["gnome-terminal ", "-- "],
+                output='screen'
+            )
+        ]
+    )
