655a662726e217e51a22aaa2c71da904f238815b
Jan Jeschke <jasiekjeschke@gmail.com>
2021-03-30 23:47:00 +0200

    działajęcy program przetwarzający tabelę DH na URDF

--- lab2/lab2/converter.py
+++ lab2/lab2/converter.py
@@ -1,3 +1,5 @@
+import math
+
 DHtable = []
 with open("../config/DHtable.txt") as file:
     for line in file:
@@ -29,7 +31,7 @@ with open("../robot/robot.urdf.xml", 'w') as file:
 
             file.write(f'<joint name="{prlink[4]}-{link[4]}" type="{link_type}">\n')
             
-            file.write(f'  <origin xyz="{prlink[0]} 0 {link[1] if link[1] != "x" else 0}" rpy="{prlink[2]} 0 {link[3] if link[3] != "x" else 0}"/>\n')
+            file.write(f'  <origin xyz="{prlink[0]} 0 {prlink[1] if prlink[1] != "x" else 0}" rpy="{prlink[2]} 0 {link[3] if link[3] != "x" else 0}"/>\n')
             file.write(f'  <parent link="{prlink[4]}"/>\n')
             file.write(f'  <child link="{link[4]}"/>\n')
             file.write(f'  <axis xyz="0 0 1"/>\n')
@@ -41,10 +43,20 @@ with open("../robot/robot.urdf.xml", 'w') as file:
         file.write(f'<link name="{link[4]}">\n')
 
         file.write('  <visual>\n')
-
-        file.write(f'    <origin xyz="{float(link[0])/2} 0 {float(link[1])/2}" rpy="0 {1.57 if float(link[0]) != 0 else 0} 0"/>\n')
+        if link[1] == "x":
+            l1 = 0
+            link_len = 0
+        else:
+            l1 = link[1]
+            link_len = (float(link[0])**2+float(l1)**2)**0.5
+        if link_len != 0:
+            angle = math.asin(float(link[0])/link_len)
+        else:
+            angle = 0
+        file.write(f'    <origin xyz="{float(link[0])/2} 0 {float(l1)/2}" rpy="0 {angle} 0"/>\n')
         file.write('      <geometry>\n')
-        file.write(f'        <cylinder radius="0.05" length="{link[0]}"/>\n')
+             
+        file.write(f'        <cylinder radius="0.05" length="{link_len}"/>\n')
         file.write('      </geometry>\n')
         file.write('      <material name="white">\n')
         file.write('        <color rgba="1 1 1 1"/>\n')
